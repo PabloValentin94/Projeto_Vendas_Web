88f18a678ffa012c90d7e72994d3ec0e7cc3e5b8
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2023-01-12 21:07:25 -0300

    Initial Commit.

---
+++ App/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+define("BASEDIR", dirname(__FILE__, 2) . "/");
+
+define("VIEWS", dirname(__FILE__) . "/View/Modules/");
+
+$_ENV["db"]["host"] = "localhost:3306";
+$_ENV["db"]["user"] = "root";
+$_ENV["db"]["password"] = "etecjau";
+$_ENV["db"]["database_name"] = "";
+
+?>
